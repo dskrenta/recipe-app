c004f4b56793693801dd929c0f2647453e56a127
dskrenta <dskrenta@gmail.com>
2018-09-09 06:45:41 -0600

    added starter recipe-app server, updated readme with sample data and recipe schema

---
+++ server/src/schema.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const { gql } = require('apollo-server');
+
+const schema = gql`
+  type Recipe {
+    title: String!
+    description: String
+    ingridients: [String!]!
+    directions: [String!]!
+    tags: [String]
+    image: String
+    images: [String]
+    url: String
+    nutrition: Nutrition
+    prepTime: Float
+    cookTime: Float
+    totalTime: Float
+    servings: Float
+    cusine: String
+    course: String
+  }
+
+  type Nutrition {
+    title: String!
+    amountPerServing: String!
+    calories: Float
+    caloriesFromFat: Float
+    totalFat: Float
+    totalFatDailyValue: Float
+    saturatedFat: Float
+    saturatedFateDailyValue: Float
+    cholesterol: Float
+    cholesterolDailyValue: Float
+    sodium: Float
+    sodiumDailyValue: Float
+    potassium: Float
+    potassiumDailyValue: Float
+    carbohydrates: Float
+    carbohydratesDailyValue: Float
+    fiber: Float
+    fiberDailyValue: Float
+    sugars: Float
+    sugarsDailyValue: Float
+    protein: Float
+    proteinDailyValue: Float
+    vitaminA: Float
+    vitaminADailyValue: Float
+    vitaminC: Float
+    vitaminCDailyValue: Float
+    iron: Float
+    ironDailyValue: Float
+  }
+
+  type Pagination {
+    offset: Int
+    limit: Int
+  }
+
+  type Query {
+    getRecipe(id: ID!): [Recipe]!
+    searchRecipes(query: String!, pagination: Pagination): [Recipe]!
+    recommendedRecipes(pagination: Pagination): [Recipe]!  
+  }
+`;
+
+module.exports = schema;
