8939061ddf8c36f26c9341244f8cdf269889300a
Bryce DesBrisay <bryce@desbrisay.com>
2018-09-22 21:52:40 -0600

    updated style of recipe card

--- app/components/Common/RecipeCard.js
+++ app/components/Common/RecipeCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
 import IconMdc from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconMd from 'react-native-vector-icons/MaterialIcons';
+import IconFa from 'react-native-vector-icons/FontAwesome';
 import IconIon from 'react-native-vector-icons/Ionicons';
 import IconEn from 'react-native-vector-icons/Entypo';
 import Star from 'react-native-star-view';
@@ -12,69 +13,101 @@ const { width, height } = Dimensions.get('window');
 
 const RecipeCard = ({ recipe }) => (
   <View style={styles.contain}>
-    <View style={styles.imageContain}>
-      <Image source={{uri: recipe.image}} style={styles.image} />
-    </View>
-    <View style={styles.titleContain}>
-      <Text style={styles.title}>{recipe.title}</Text>
-      <View style={styles.ratingRow}>
-        <Star score={recipe.rating} style={styles.ratingStars} />
+    <View style={styles.overflow}>
+      <View style={styles.imageContain}>
+        <Image source={{uri: recipe.image}} style={styles.image} />
+        <View style={styles.saveContain}>
+          <IconFa name="bookmark-o" size={30} color="#fff" />
+        </View>
+        <View style={styles.ratingContain}>
+          <View style={styles.ratingRow}>
+            <IconMd name="star" size={25} color="orange" />
+            <Text style={styles.ratingText}>{recipe.rating}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.titleContain}>
+        {/*<View style={styles.ratingCircle}>
+          <View style={styles.ratingRow}>
+            <IconMd name="star" size={25} color="orange" />
+            <Text style={styles.ratingText}>{recipe.rating}</Text>
+          </View>
+        </View>*/}
+        <Text style={styles.title}>{recipe.title}</Text>
+        <View style={styles.titleSubrow}>
+          <View style={styles.courseTag}>
+            <Text style={styles.course}>{recipe.course}</Text>
+          </View>
+          <Text style={styles.cuisine}>{recipe.cuisine} Cuisine</Text>
+        </View>
+      </View>
+      {/*<View style={styles.ratingRow}>
+        <IconMd name="star" size={25} color="orange" style={styles.ratingStars} />
         <Text style={styles.ratingText}>{recipe.rating}</Text>
+      </View>*/}
+      {/*<View style={styles.colorRow}>
+        <View style={styles.colorItem}>
+          <Text style={styles.colorItemText}>Easy</Text>
+        </View>
+        <View style={[styles.colorItem, {borderRightWidth: 1, borderLeftWidth: 1}]}>
+          <Text style={styles.colorItemText}>{recipe.cuisine}</Text>
+        </View>
+        <View style={styles.colorItem}>
+          <Text style={styles.colorItemText}>{recipe.course}</Text>
+        </View>
+      </View>*/}
+      <View style={styles.bottomRow}>
+        <View style={styles.bottomItem}>
+          <Text style={styles.statText}>{recipe.totalTime}</Text>
+          <Text style={styles.statSpan}>Minutes</Text>
+        </View>
+        <View style={styles.bottomItem}>
+          <Text style={styles.statText}>{recipe.servings}</Text>
+          <Text style={styles.statSpan}>Servings</Text>
+        </View>
+        <View style={styles.bottomItem}>
+          <Text style={styles.statText}>{recipe.nutrition.calories}</Text>
+          <Text style={styles.statSpan}>Calories</Text>
+        </View>
       </View>
+      <View style={styles.descContain}>
+        <Text style={styles.descTitle}>Description</Text>
+        <Text numberOfLines={3}>{recipe.description}</Text>
+      </View>
+      {/*<View style={styles.row}>
+        <IconIon name="md-stopwatch" size={20} color="#333" style={{marginRight: 5}} />
+        <Text style={styles.statSpan1}>{recipe.totalTime}</Text>
+        <Text style={styles.statSpan}> Minutes</Text>
+        <IconIon name="md-restaurant" size={20} color="#333" style={{marginLeft: 15, marginRight: 5}} />
+        <Text style={styles.statSpan1}>{recipe.servings}</Text>
+        <Text style={styles.statSpan}> Servings</Text>
+      </View>*/}
+      {/*<View style={styles.colorRow}>
+        <View style={[styles.colorItem, {backgroundColor: '#66e599'}]}>
+          <Text style={styles.colorItemText}>EASY</Text>
+        </View>
+        <View style={[styles.colorItem, {backgroundColor: '#3bde7c'}]}>
+          <Text style={styles.colorItemText}>{recipe.cuisine.toUpperCase()}</Text>
+        </View>
+        <View style={[styles.colorItem, {backgroundColor: '#2c6'}]}>
+          <Text style={styles.colorItemText}>{recipe.course.toUpperCase()}</Text>
+        </View>
+      </View>*/}
+      {/*<View style={styles.bottomRow}>
+        <View style={styles.bottomItem}>
+          <IconIon name="md-stopwatch" size={30} color="#2c6" />
+          <Text style={styles.statSpan}>{recipe.totalTime}<Text style={styles.statSpan}> minutes</Text></Text>
+        </View>
+        <View style={styles.bottomItem}>
+          <IconMd name="restaurant" size={30} color="#2c6" />
+          <Text style={styles.statSpan}>{recipe.servings}<Text style={styles.statSpan}> servings</Text></Text>
+        </View>
+        <View style={styles.bottomItem}>
+          <IconMdc name="fire" size={30} color="#2c6" />
+          <Text style={styles.statSpan}>{recipe.nutrition.calories}<Text style={styles.statSpan}> calories</Text></Text>
+        </View>
+      </View>*/}
     </View>
-    {/*<View style={styles.colorRow}>
-      <View style={styles.colorItem}>
-        <Text style={styles.colorItemText}>EASY</Text>
-      </View>
-      <View style={[styles.colorItem, {borderRightWidth: 1, borderLeftWidth: 1}]}>
-        <Text style={styles.colorItemText}>{recipe.cuisine.toUpperCase()}</Text>
-      </View>
-      <View style={styles.colorItem}>
-        <Text style={styles.colorItemText}>{recipe.course.toUpperCase()}</Text>
-      </View>
-    </View>*/}
-    <View style={styles.colorRow}>
-      <View style={[styles.colorItem, {backgroundColor: '#66e599'}]}>
-        <Text style={styles.colorItemText}>EASY</Text>
-      </View>
-      <View style={[styles.colorItem, {backgroundColor: '#3bde7c'}]}>
-        <Text style={styles.colorItemText}>{recipe.cuisine.toUpperCase()}</Text>
-      </View>
-      <View style={[styles.colorItem, {backgroundColor: '#2c6'}]}>
-        <Text style={styles.colorItemText}>{recipe.course.toUpperCase()}</Text>
-      </View>
-    </View>
-    <View style={styles.bottomRow}>
-      <View style={styles.bottomItem}>
-        <Text style={styles.statText}>{recipe.totalTime}</Text>
-        <Text style={styles.statSpan}>Minutes</Text>
-      </View>
-      <View style={styles.bottomItem}>
-        <Text style={styles.statText}>{recipe.servings}</Text>
-        <Text style={styles.statSpan}>Servings</Text>
-      </View>
-      <View style={styles.bottomItem}>
-        <Text style={styles.statText}>{recipe.nutrition.calories}</Text>
-        <Text style={styles.statSpan}>Calories</Text>
-      </View>
-    </View>
-    <View style={styles.descContain}>
-      <Text numberOfLines={3}>{recipe.description}</Text>
-    </View>
-    {/*<View style={styles.bottomRow}>
-      <View style={styles.bottomItem}>
-        <IconIon name="md-stopwatch" size={30} color="#2c6" />
-        <Text style={styles.statSpan}>{recipe.totalTime}<Text style={styles.statSpan}> min</Text></Text>
-      </View>
-      <View style={styles.bottomItem}>
-        <IconIon name="md-ribbon" size={30} color="#2c6" />
-        <Text style={styles.statSpan}>{recipe.rating}<Text style={styles.statSpan}>/5</Text></Text>
-      </View>
-      <View style={styles.bottomItem}>
-        <IconMdc name="fire" size={30} color="#2c6" />
-        <Text style={styles.statSpan}>{recipe.nutrition.calories}<Text style={styles.statSpan}> cal</Text></Text>
-      </View>
-    </View>*/}
   </View>
 );
 
@@ -85,49 +118,117 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 10,
     backgroundColor: 'white',
-    borderRadius: 10,
+    borderRadius: 15,
     width: width - 30,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.15,
     shadowRadius: 3,
     elevation: 2
+  },
+  overflow: {
+    overflow: 'hidden',
+    flex: 1
   },
   imageContain: {
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     overflow: 'hidden',
-    flex: 1
+    flex: 1,
+    position: 'relative'
   },
   image: {
     width: '100%',
     height: (height - iPhoneStyle(220, 180, 180)) / 2,
     flex: 1
   },
+  saveContain: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+  },
   titleContain: {
-    padding: 20,
-    paddingBottom: 5
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative'
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#333'
+  },
+  titleSubrow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    justifyContent: 'flex-start',
+    marginTop: 5
+  },
+  courseTag: {
+    paddingVertical: 3,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    backgroundColor: '#2c6',
+    marginRight: 8
+  },
+  course: {
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  cuisine: {
+    fontSize: 16,
+    color: '#333'
+  },
+  ratingCircle: {
+    position: 'absolute',
+    top: -20,
+    right: 20,
+    height: 40,
+    width: 80,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 3,
+    elevation: 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ratingContain: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    height: 40,
+    width: 80,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 3,
+    elevation: 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   ratingRow: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start'
-  },
-  ratingStars: {
-    height: 20,
-    width: 100,
+    justifyContent: 'flex-start',
+    flex: 0
   },
   ratingText: {
-    fontSize: 16,
-    marginHorizontal: 6,
-    marginBottom: 3,
-    marginTop: 4
+    fontSize: 18,
+    color: '#666',
+    fontWeight: 'bold',
+    marginHorizontal: 5
   },
   colorRow: {
     display: 'flex',
@@ -137,7 +238,7 @@ const styles = StyleSheet.create({
   },
   colorItem: {
     flex: 1,
-    //xflexGrow: 1,
+    //flexGrow: 1,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -147,8 +248,8 @@ const styles = StyleSheet.create({
     marginVertical: 10
   },
   colorItemText: {
-    fontWeight: '900',
-    color: '#fff',
+    fontWeight: 'bold',
+    color: '#444',
     textAlign: 'center'
   },
   bottomRow: {
@@ -156,30 +257,51 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'stretch',
     justifyContent: 'space-between',
+    paddingLeft: 20
   },
   bottomItem: {
     flex: 1,
     flexGrow: 1,
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
-    paddingTop: 5,
-    paddingBottom: 15
+    paddingTop: 8,
+    paddingBottom: 10
   },
   statText: {
-    marginBottom: 5,
-    fontSize: 20,
+    marginBottom: 3,
+    fontSize: 18,
     fontWeight: 'bold',
     color: '#1a5'
   },
   statSpan: {
-    color: '#666'
+    color: '#888',
+    fontSize: 14
   },
   descContain: {
     padding: 20,
     paddingTop: 0,
     paddingBottom: 20
-  }
+  },
+  descTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+    paddingBottom: 20
+  },
+  statSpan1: {
+    paddingLeft: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#2c6'
+  } 
 });
 
 export default RecipeCard;
--- app/components/Common/SearchWrap.js
+++ app/components/Common/SearchWrap.js
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.15,
     shadowRadius: 3,
     elevation: 2
   },
