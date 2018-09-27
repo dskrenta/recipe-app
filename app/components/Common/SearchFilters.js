563eb2b518fc8e3e0e89a32ebb2a4427b1e59422
Bryce DesBrisay <bryce@desbrisay.com>
2018-09-27 15:08:39 -0600

    created search filters component

---
+++ app/components/Common/SearchFilters.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+
+class SearchFilters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breakfast: false,
+      lunch: false,
+      dinner: false,
+      snacks: false,
+      time30: false,
+      time60: false,
+      time60p: false,
+      serves2: false,
+      serves4: false,
+      serves6: false,
+      serves8: false,
+      calMin: 0,
+      calMax: 2000
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.filterContain}>
+        <Text style={styles.filterTitle}>Sort by Course</Text>
+        <View style={styles.filterRow}>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.snacks && {backgroundColor: '#92edb6'}]}
+            onPress={() => {this.setState({ snacks: !this.state.snacks })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>Snacks</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.breakfast && {backgroundColor: '#7ce9a8'}]}
+            onPress={() => {this.setState({ breakfast: !this.state.breakfast })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>Breakfast</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.lunch && {backgroundColor: '#66e599'}]}
+            onPress={() => {this.setState({ lunch: !this.state.lunch })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>Lunch</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.dinner && {backgroundColor: '#51e18b'}]}
+            onPress={() => {this.setState({ dinner: !this.state.dinner })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>Dinner</Text>
+          </TouchableHighlight>
+        </View>
+        <Text style={styles.filterTitle}>Sort by Time</Text>
+        <View style={styles.filterRow}>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.time30 && {backgroundColor: '#92edb6'}]}
+            onPress={() => {this.setState({ time30: !this.state.time30 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>30 min</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.time60 && {backgroundColor: '#7ce9a8'}]}
+            onPress={() => {this.setState({ time60: !this.state.time60 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>60 min</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.time60p && {backgroundColor: '#66e599'}]}
+            onPress={() => {this.setState({ time60p: !this.state.time60p })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>60+ min</Text>
+          </TouchableHighlight>
+        </View>
+        <Text style={styles.filterTitle}>Sort by Servings</Text>
+        <View style={styles.filterRow}>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.serves2 && {backgroundColor: '#92edb6'}]}
+            onPress={() => {this.setState({ serves2: !this.state.serves2 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>2</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.serves4 && {backgroundColor: '#7ce9a8'}]}
+            onPress={() => {this.setState({ serves4: !this.state.serves4 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>4</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.serves6 && {backgroundColor: '#66e599'}]}
+            onPress={() => {this.setState({ serves6: !this.state.serves6 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>6</Text>
+          </TouchableHighlight>
+          <TouchableHighlight 
+            style={[styles.filterToggle, this.state.serves8 && {backgroundColor: '#51e18b'}]}
+            onPress={() => {this.setState({ serves8: !this.state.serves8 })}}
+            underlayColor="#bef4d3"
+          >
+            <Text style={styles.toggleText}>8+</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  filterContain: {
+    flex: 1,
+    height: '100%',
+    padding: 20,
+    paddingBottom: 50,
+  },
+  filterTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    marginTop: 5,
+    color: '#333'
+  },
+  filterRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    backgroundColor: '#d3f8e2',
+    marginHorizontal: -20,
+    marginBottom: 15
+  },
+  filterToggle: {
+    flex: 1,
+    paddingVertical: 15,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d3f8e2'
+  },
+  toggleText: {
+    color: '#333'
+  }
+})
+
+export default SearchFilters;
--- app/components/Common/SearchWrap.js
+++ app/components/Common/SearchWrap.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {
   View,
   ScrollView,
-  Text,
   TouchableHighlight,
   StyleSheet,
   TextInput,
-  Animated,
-  Slider
+  Animated
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import SearchFilters from './SearchFilters';
 import { iPhoneStyle } from '../../utils/iPhoneStyle';
 
 class SearchWrap extends React.Component {
@@ -20,20 +20,7 @@ class SearchWrap extends React.Component {
       value: '',
       showSearch: false,
       backIcon: false,
-      fade: new Animated.Value(0),
-      breakfast: false,
-      lunch: false,
-      dinner: false,
-      snacks: false,
-      time30: false,
-      time60: false,
-      time60p: false,
-      serves2: false,
-      serves4: false,
-      serves6: false,
-      serves8: false,
-      calMin: 0,
-      calMax: 2000
+      fade: new Animated.Value(0)
     }
   }
 
@@ -99,95 +86,7 @@ class SearchWrap extends React.Component {
           ]}
         >
           <ScrollView style={styles.filterScroll}>
-            <View style={styles.filterContain}>
-              <Text style={styles.filterTitle}>Sort by Course</Text>
-              <View style={styles.filterRow}>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.snacks && {backgroundColor: '#92edb6'}]}
-                  onPress={() => {this.setState({ snacks: !this.state.snacks })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>Snacks</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.breakfast && {backgroundColor: '#7ce9a8'}]}
-                  onPress={() => {this.setState({ breakfast: !this.state.breakfast })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>Breakfast</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.lunch && {backgroundColor: '#66e599'}]}
-                  onPress={() => {this.setState({ lunch: !this.state.lunch })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>Lunch</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.dinner && {backgroundColor: '#51e18b'}]}
-                  onPress={() => {this.setState({ dinner: !this.state.dinner })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>Dinner</Text>
-                </TouchableHighlight>
-              </View>
-              <Text style={styles.filterTitle}>Sort by Time</Text>
-              <View style={styles.filterRow}>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.time30 && {backgroundColor: '#92edb6'}]}
-                  onPress={() => {this.setState({ time30: !this.state.time30 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>30 min</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.time60 && {backgroundColor: '#7ce9a8'}]}
-                  onPress={() => {this.setState({ time60: !this.state.time60 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>60 min</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.time60p && {backgroundColor: '#66e599'}]}
-                  onPress={() => {this.setState({ time60p: !this.state.time60p })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>60+ min</Text>
-                </TouchableHighlight>
-              </View>
-              <Text style={styles.filterTitle}>Sort by Servings</Text>
-              <View style={styles.filterRow}>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.serves2 && {backgroundColor: '#92edb6'}]}
-                  onPress={() => {this.setState({ serves2: !this.state.serves2 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>2</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.serves4 && {backgroundColor: '#7ce9a8'}]}
-                  onPress={() => {this.setState({ serves4: !this.state.serves4 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>4</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.serves6 && {backgroundColor: '#66e599'}]}
-                  onPress={() => {this.setState({ serves6: !this.state.serves6 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>6</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                  style={[styles.filterToggle, this.state.serves8 && {backgroundColor: '#51e18b'}]}
-                  onPress={() => {this.setState({ serves8: !this.state.serves8 })}}
-                  underlayColor="#bef4d3"
-                >
-                  <Text style={styles.toggleText}>8+</Text>
-                </TouchableHighlight>
-              </View>
-              <Slider minimumValue={0} maximumValue={2000} />
-            </View>
+            <SearchFilters />
           </ScrollView>
         </Animated.View>
         <View style={styles.children}>
@@ -247,38 +146,6 @@ const styles = StyleSheet.create({
   filterScroll: {
     flex: 1,
     height: '100%'
-  },
-  filterContain: {
-    flex: 1,
-    height: '100%',
-    padding: 20,
-    paddingBottom: 50,
-  },
-  filterTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    marginTop: 5,
-    color: '#333'
-  },
-  filterRow: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    backgroundColor: '#d3f8e2',
-    marginHorizontal: -20,
-    marginBottom: 15
-  },
-  filterToggle: {
-    flex: 1,
-    paddingVertical: 15,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#d3f8e2'
-  },
-  toggleText: {
-    color: '#333'
   },
   children: {
     position: 'absolute',
