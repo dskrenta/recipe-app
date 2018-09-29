e78e623e0bf7cf209f6b54bfcc735825ee2d60d2
dskrenta <dskrenta@gmail.com>
2018-09-29 12:18:11 -0700

    grabRecipe

--- scraping/foodNetwork/index.js
+++ scraping/foodNetwork/index.js
@@ -164,6 +164,7 @@ scraper.addTarget({
 });
 */
 
+/*
 async function grabRecipe({ url, browser }) {
   try {
     const page = await browser.newPage();
@@ -191,6 +192,56 @@ async function grabRecipe({ url, browser }) {
         // image,
         tags,
         ingredients
+      };
+    });
+
+    console.log(data);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+*/
+
+async function grabRecipe({ url, browser }) {
+  try {
+    const page = await browser.newPage();
+    const status = await page.goto(url);
+
+    if (!status.ok) {
+      console.error(`Cannot open ${url}`);
+      throw new Error();
+    }
+
+    const data = await page.evaluate(() => {
+      const title = document.querySelector('span.o-AssetTitle__a-HeadlineText').textContent;
+      const rating = parseInt(document.querySelector('span.gig-rating-stars').getAttribute('title').match(/^\d+/g)[0]);
+      const level = document.querySelector('span.o-RecipeInfo__a-Description').textContent;
+      const servings = parseInt(document.querySelector('ul.o-RecipeInfo__m-Yield > li > span.o-RecipeInfo__a-Description').textContent.match(/\d+/g)[0]);
+      const tags = Array.from(document.querySelectorAll('a.o-Capsule__a-Tag.a-Tag')).map(node => node.textContent);
+
+      const times = document.querySelector('ul.o-RecipeInfo__m-Level').children[1].children[1].textContent.match(/\d+/g).map(elem => parseInt(elem));
+      let totalTime;
+      if (times.length > 1) {
+        totalTime = (times[0] * 60) + times[1];
+      }
+      else {
+        times = times[0];
+      }
+
+      let directions = [];
+      Array.from(document.querySelector('div.o-Method__m-Body').children).map(node => node.tagName === 'H4' ? `#${node.textContent.trim()}` : Array.from(node.children).map(node => node.textContent.trim())).forEach(direction => (typeof direction === 'string') ? directions.push(direction) : directions = [...directions, ...direction]);
+      const ingredients = Array.from(document.querySelector('div.o-Ingredients__m-Body').children).map(node => node.tagName === 'H6' ? `#${node.textContent.trim()}` : node.textContent);;
+
+      return {
+        title,
+        rating,
+        level,
+        servings,
+        totalTime,
+        directions,
+        ingredients,
+        tags
       };
     });
 
