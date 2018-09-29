19a8c39aeabc527715b808ad2536578b658e29cb
Bryce DesBrisay <bryce@desbrisay.com>
2018-09-29 00:36:01 -0700

    updated style of shopping list

--- app/components/ShoppingList/ShoppingList.js
+++ app/components/ShoppingList/ShoppingList.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableHighlight, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, ScrollView, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconFa from 'react-native-vector-icons/FontAwesome';
 
 import SearchWrap from '../Common/SearchWrap';
 
 list = [
   '10 to 12 cups reduced-sodium or homemade chicken broth*',
-  '2 tablespoons olive oil',
-  '2 tablespoons plus 1 tsp. butter, divided',
   '1 medium onion, chopped (about 1 1/2 cups)',
   '2 1/2 cups risotto rice (see note)',
   '3/4 cup dry white wine',
-  'About 1/2 tsp. salt',
   '1/2 cup freshly grated parmesan cheese, plus more for serving',
-  '1/2 teaspoon freshly ground black pepper',
   '1/4 cup chopped flat-leaf parsley'
 ]
 
@@ -24,7 +21,8 @@ class ShoppingList extends React.Component {
 
     this.state = {
       list: list,
-      checks: Array(list.length).fill(0)
+      checks: Array(list.length).fill(0),
+      value: ''
     }
   }
 
@@ -37,9 +35,14 @@ class ShoppingList extends React.Component {
   delete = (i) => {
     const newChecks = this.state.checks;
     const newList = this.state.list;
-    newChecks.splice(i,i);
-    newList.splice(i,i);
+    newChecks.splice(i,1);
+    newList.splice(i,1);
     this.setState({ checks: newChecks, list: newList });
+  }
+
+  onSubmit = () => {
+    const newList = [...this.state.list, this.state.value]
+    this.setState({ value: '', list: newList })
   }
 
   render() {
@@ -57,27 +60,47 @@ class ShoppingList extends React.Component {
                   <TouchableHighlight
                     onPress={() => {this.toggleCheck(i)}}
                     underlayColor="transparent"
-                    style={styles.checkbox}
+                    style={{flex: 1}}
                   >
-                    <Icon 
-                      name={this.state.checks[i] ? "checkbox-marked" : "checkbox-blank-outline"} 
-                      color={this.state.checks[i] ? '#2c6' : '#333'} 
-                      size={20} 
-                    />
+                    <View style={styles.checkbox}>
+                      <Icon 
+                        name={this.state.checks[i] ? "checkbox-marked" : "checkbox-blank-outline"} 
+                        color={this.state.checks[i] ? '#2c6' : '#333'} 
+                        size={20} 
+                      />
+                      <View style={styles.itemTextContain}>
+                        <Text style={[styles.itemText, this.state.checks[i] && {textDecorationLine: 'line-through', color: '#1a5'}]}>{item}</Text>
+                      </View>
+                    </View>
                   </TouchableHighlight>
-                  <View style={styles.itemTextContain}>
-                    <Text style={styles.itemText}>{item}</Text>
-                  </View>
                   <TouchableHighlight
                     onPress={() => {this.delete(i)}}
                     underlayColor="transparent"
                     style={styles.delete}
                   >
-                    <Icon name="close" color="#aaa" size={15} />
+                    <Icon name="close" color="#888" size={15} />
                   </TouchableHighlight>
                 </View>
               ))}
             </ScrollView>
+            <View style={styles.inputContain}>
+              <View style={styles.searchBox}>
+                <TextInput 
+                  placeholder="Add Ingredient"
+                  style={styles.addInput}
+                  value={this.state.value}
+                  onChangeText={(value) => {this.setState({ value })}}
+                  onSubmitEditing={this.onSubmit}
+                  underlineColorAndroid="transparent"
+                />
+                <TouchableHighlight
+                  onPress={this.addItem}
+                  underlayColor="transparent"
+                >
+                  <IconFa name="plus" color="#2c6" size={20} style={styles.add}/>
+                </TouchableHighlight>
+              </View>
+            </View>
           </View>
         </SearchWrap>
       </SafeAreaView>
@@ -101,21 +124,24 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   scrollView: {
-    paddingTop: 10
+    paddingTop: 20
   },
   item: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'flex-start',
     justifyContent: 'space-between',
-    paddingVertical: 10,
+    paddingVertical: 8,
   },
   checkbox: {
-    paddingRight: 15,
-    paddingLeft: 20
+    paddingLeft: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
   },
   itemTextContain: {
     flex: 1,
+    paddingLeft: 20
   },
   itemText: {
     fontSize: 16,
@@ -125,6 +151,36 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
     paddingVertical: 2.5,
     paddingRight: 20
+  },
+  inputContain: {
+    width: '100%',
+    height: 75,
+    padding: 15,
+    paddingBottom: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#eee'
+  },
+  searchBox: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 3,
+    elevation: 2,
+    paddingLeft: 20
+  },
+  add: {
+    marginHorizontal: 25,
+    marginVertical: 10 
+  },
+  addInput: {
+    flex: 1,
+    paddingVertical: 10
   }
 });
 
