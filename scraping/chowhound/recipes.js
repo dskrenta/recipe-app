0a8f16ab072ffcda95872cfcedbc3ed5e055a7c3
Bryce DesBrisay <bryce@desbrisay.com>
2018-10-01 20:20:05 -0700

    got all recipe info except for nutrition

--- scraping/chowhound/recipes.js
+++ scraping/chowhound/recipes.js
@@ -22,9 +22,7 @@ async function getRecipe({ url, browser }) {
   try {
     const page = await browser.newPage();
     const status = await page.goto(url);
-    let recipe = {
-      name: 'recipe'
-    };
+    let recipe = {};
 
     if (!status.ok) {
       console.error(`Cannot open ${url}`);
@@ -37,21 +35,83 @@ async function getRecipe({ url, browser }) {
 
       const title = document.querySelector('.fr_r_info h1').innerHTML;
       if (title) { obj.title = title; }
+      
       const tags = Array.from(document.querySelectorAll('.freyja_fulltags .freyja_tag')).map(tag => tag.getAttribute('data-name'));
       if (tags) { obj.tags = tags; }
+
+      const image = document.querySelector('.fr_objfitimg').getAttribute('src');
+      if (image) { obj.image = image; }
+      
       const serves = parseFloat(document.querySelector('.frr_serves').innerHTML);
       if (serves) { obj.servings = serves; }
+      
       const rating = parseFloat((document.querySelector('.chow_rating_display > .viewport').offsetWidth * 5 / document.querySelector('.chow_rating_display > .lower_layer').offsetWidth).toFixed(1));
       if (rating) { obj.rating = rating; }
+      
       const description = Array.from(document.querySelectorAll('.frr_summary > p')).map(node => node.innerText).join("\n");
       if (description) { obj.description = description; }
+      
+      const totalTime = document.querySelector('.frr_totaltime > time').innerText;
+      if (totalTime) {
+        let total = 0;
+        const newTime = totalTime.split(',')[0];
+        const times = newTime.match(/\d{1,2} [h|m]/g);
+        for (let time of times) {
+          if (time[time.length - 1] === 'h') {
+            total += parseFloat(time.match(/\d{1,2}/) * 60);
+          }
+          else {
+            total += parseFloat(time.match(/\d{1,2}/))
+          }
+        }
+        obj.totalTime = total
+      }
+
+      let ingredients = [];
+      const ingredientList = document.querySelector('.freyja_box81').children;
+      if (ingredientList) {
+        for (let item of ingredientList) {
+          if (item.tagName === 'UL') {
+            const lis = item.children;
+            for (let child of lis) {
+              ingredients.push(child.innerText);
+            }
+          }
+          else if (item.tagName === 'H4') {
+            ingredients.push(`#${item.innerText}`);
+          }
+        }
+        obj.ingredients = ingredients;
+      }
+
+      let directions = [];
+      const directionList = document.querySelector('.freyja_box82 > .frr_wrap').children;
+      if (directionList) {
+        for (let item of directionList) {
+          if (item.tagName === 'OL') {
+            const lis = item.children;
+            for (let child of lis) {
+              const newstr = child.innerText.substring(1);
+              directions.push(newstr);
+            }
+          }
+          else if (item.tagName === 'STRONG') {
+            directions.push(`#${item.innerText}`);
+          }
+          else if (item.tagName === 'P') {
+            directions.push(`#${item.innerText}`);
+          }
+        }
+        obj.directions = directions;
+      }
+
+      obj.url = url;
 
       return obj;
     });
 
     recipe = result;
     console.log(recipe);
-    console.log(recipe.rating)
 
     /*
     if (recipe !== undefined) {
