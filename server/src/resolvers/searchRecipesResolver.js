e1c51d8ade8b860a1910aa892ce9044aa9f7ac84
dskrenta <dskrenta@gmail.com>
2018-10-20 00:54:54 -0600

    fixed graphql schema with recipe resolvers

--- server/src/resolvers/searchRecipesResolver.js
+++ server/src/resolvers/searchRecipesResolver.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const flattenResponse = require('../utils/flattenResponse');
 const searchRecipes = require('../models/searchRecipes');
 
 async function searchRecipesResolver({ client, args: { query, pagination } }) {
   try {
     const recipes = await searchRecipes({ client, query, pagination });
-    return flattenResponse({ res: recipes, array: true, total: true });
+    return recipes;
   }
   catch (error) {
     console.error(error);
